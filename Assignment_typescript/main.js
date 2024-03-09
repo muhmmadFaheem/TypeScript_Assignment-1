5f10850487a66168871fcb2e595b43acb3200306
Muhammad Faheem <157746418+muhmmadFaheem@users.noreply.github.com>
2024-03-10 01:28:48 +0500

    Add files via upload

---
+++ Assignment_typescript/main.js
@@ -0,0 +1,49 @@
+"use strict";
+//100_ Days_Of_ Codding_ Challenge_of_TypeScripts....
+//Day No.1..
+let name1 = "Faheem";
+console.log("Hello " + name1 + ", would you like to learn some Typescript today?");
+//show name in lowercase
+let name2 = "Faheem";
+console.log(name2.toLowerCase());
+//show name in higercase
+console.log(name2.toUpperCase());
+console.log(name2.charAt(0).toUpperCase() + name2.slice(1).toLowerCase());
+//Day No.2...
+console.log("Elon Musk once said", "Dont confuse schooling with education.I didn't go to Harvard but the people that work for me did.");
+let famous_person = "Elon Musk";
+let mesg = famous_person + "once said Dont confuse schooling with education.I didnt go to Harvard but the people that work for me did.";
+console.log(mesg);
+let name3 = "\t\n Faheem\t\n";
+console.log(name3);
+//name with whitespace
+console.log(name3.trim());
+//name without whitespace
+//  Day No.3..
+//Question 7..
+console.log(5 + 3); //addition
+console.log(10 - 2); //substraction
+console.log(4 * 2); //multiplication
+console.log(16 / 2); //division
+//Question 8..
+console.log(17 + 3); //addition
+console.log(40 - 20); //substraction
+console.log(4 * 5); //multiplication
+console.log(40 / 2); //division
+//Question 9..
+let favornum = 4;
+console.log("My favorite number is  " + favornum + ".");
+// Day 4...
+//Question 10...
+let myName = "Faheem";
+console.log("Hello " + myName + " would you like to learn some TypeScript today ?");
+//Question 11..
+let names = ["Babar", "Rizwan", "Saim"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+//Question 12..
+let names1 = ["Junaid", "Jamal", "Jamshed"];
+for (let name of names) {
+    console.log("Hello " + name + " would you like to learn some TypeScript");
+}
